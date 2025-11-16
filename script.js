a8fe987bbf8acb6fd270b2b24065e1a6f46bb61f
shrub <shrub719@gmail.com>
2025-11-16 19:29:58 +0000

    add test web page

---
+++ script.js
@@ -0,0 +1,11 @@
+import init, { upper } from "./pkg/lambda.js";
+
+async function run() {
+    await init();
+}
+
+const textInput = document.getElementById("text-input");
+
+textInput.addEventListener("input", (e) => {
+    e.target.value = upper(e.target.value);
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub fn upper(string: &str) -> String {
+    let string_upper = string.to_ascii_uppercase();
+    string_upper
+}
