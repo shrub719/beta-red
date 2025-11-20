082c8ecbf330f96100cfd0a7d73841cd077df33e
shrub <shrub719@gmail.com>
2025-11-20 19:15:14 +0000

    html tweak

--- site/index.js
+++ site/index.js
@@ -1,16 +1,24 @@
 import init, { parse } from "../pkg/lambda.js";
 
+function handleEdit(e) {
+    if (e.target.value === "") {
+        output.innerText = "type something!";
+        return;
+    }
+
+    try {
+        let result = parse(e.target.value);
+        output.innerText = JSON.stringify(result, null, 2);
+    } catch (e) {
+        output.innerText = e;
+    }
+}
+
 init().then(() => {
     const input = document.getElementById("input");
     const output = document.getElementById("output");
 
-    input.addEventListener("input", (e) => {
-        try {
-            let result = parse(e.target.value);
-            output.innerText = JSON.stringify(result, null, 2);
-        } catch (e) {
-            output.innerText = e;
-        }
-    });
+    input.addEventListener("input", handleEdit);
+    input.dispatchEvent(new Event("input"));
 });
 
